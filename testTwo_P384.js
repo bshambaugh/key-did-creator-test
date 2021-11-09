fc111e5aebcb2b33c055dd1ecb36ce2dd8754ff3
Brent Shambaugh <brent.shambaugh@gmail.com>
2021-11-09 17:04:51 -0600

    add P-384 and P-521 curves

---
+++ testTwo_P384.js
@@ -0,0 +1,20 @@
+// https://github.com/bshambaugh/did-key-creator 
+
+import * as name from './did-key-creator/lib/encodeDIDkey.js'
+import * as utils from './did-key-creator/lib/utils.js'
+import * as u8a from 'uint8arrays'
+import ec from 'elliptic'
+var EC = ec.ec;
+
+var multicodecName = 'p384-pub';
+
+const  ecurve = new EC('p384');
+const  key = ecurve.genKeyPair();
+const  pubPoint = key.getPublic('hex');
+
+const rawKey = utils.rawKeyInHexfromUncompressed(pubPoint);
+const compressedKey = utils.compressedKeyInHexfromRaw(rawKey);
+
+const publicKey2 = u8a.fromString(compressedKey,'base16');
+console.log(name.encodeDIDfromHexString(multicodecName,compressedKey));
+console.log(name.encodeDIDfromBytes(multicodecName,publicKey2));
